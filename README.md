160842674ebf1a5b9946b5c0e8ed0b6bb0c6aefb
antoine emmanuel bachmann <antebachmann@yahoo.com>
2022-12-07 12:48:47 +0100

    training works on stable env

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 e-CARE benchmarking dataset and adjoined generative code
+
+the code in e-CARE-main was directly downloaded from the e-CARE github repo (https://github.com/Waste-Wood/e-CARE) last taken on 05/12/2022
---
+++ main.py
@@ -0,0 +1,89 @@
+import torch
+from transformers import GPT2Tokenizer, GPT2Model, AdamW, get_linear_schedule_with_warmup
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm, trange
+
+"""
+
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+generator = pipeline('text-generation', model='gpt2')
+set_seed(42)
+print(generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5))
+"""
+
+
+# Accumulated batch size (since GPT2 is so big)
+def pack_tensor(new_tensor, packed_tensor, max_seq_len):
+    if packed_tensor is None:
+        return new_tensor, True, None
+    if new_tensor.size()[1] + packed_tensor.size()[1] > max_seq_len:
+        return packed_tensor, False, new_tensor
+    else:
+        packed_tensor = torch.cat([new_tensor, packed_tensor[:, 1:]], dim=1)
+        return packed_tensor, True, None
+
+
+def train(
+        dataset, model, tokenizer,
+        batch_size=16, epochs=5, lr=2e-5,
+        max_seq_len=400, warmup_steps=200,
+        gpt2_type="gpt2", output_dir=".", output_prefix="wreckgar",
+        test_mode=False
+):
+    acc_steps = 100
+    device = torch.device("cuda")
+    model = model.cuda()
+    model.train()
+
+    optimizer = AdamW(model.parameters(), lr=lr)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=-1
+    )
+
+    train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    loss = 0
+    accumulating_batch_count = 0
+    input_tensor = None
+
+    for epoch in range(epochs):
+
+        print(f"Training epoch {epoch}")
+        print(loss)
+        for idx, entry in tqdm(enumerate(train_dataloader)):
+            (input_tensor, carry_on, remainder) = pack_tensor(entry, input_tensor, 768)
+
+            if carry_on and idx != len(train_dataloader) - 1:
+                continue
+
+            input_tensor = input_tensor.to(device)
+            outputs = model(input_tensor)
+            loss = outputs[0]
+
+            if (accumulating_batch_count % batch_size) == 0:
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                model.zero_grad()
+
+            accumulating_batch_count += 1
+            input_tensor = None
+    return model
+
+
+# use jsonlines, TODO
+with open('C:/Users/Antoine/Desktop/UNI/Master/Sem2/ML/MLP2/e-CARE-main/dataset/Explanation_Generation/train.jsonl',
+          'r') as json_file:
+    json_list = list(json_file)
+
+json_list = [x.replace('"', '').replace(",", '').replace("{", '').replace("}", '').split(' ', 2)[2] for x in json_list]
+print(json_list[0])
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2Model.from_pretrained('gpt2')
+dataset = []
+for elem in json_list:
+    dataset.append(torch.tensor(tokenizer.encode(elem)))
+
+model = train(dataset, model, tokenizer)
